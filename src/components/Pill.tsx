02c9d6bdb6dee52bc6b6daea4044a4f3b2813bf9
Nautica <ThisIsNautica@Outlook.com>
2025-09-04 17:44:11 -0500

    - Added the Featured parks section - Added The Captured Moments Section - Added the Get Connected Section

--- src/components/Pill.tsx
+++ src/components/Pill.tsx
@@ -4,16 +4,18 @@ interface PillProps {
   pillText: string;
   textColor?: string;
   backgroundColor?: string;
+  className?: string;
 }
 
 export const Pill = ({
   pillText,
   textColor = "#36634d",
   backgroundColor = "",
+  className = "",
 }: PillProps) => {
   return (
     <div
-      className="rounded-full w-fit mb-4 px-3 py-1 flex "
+      className={`rounded-full w-fit mb-4 px-3 py-1 flex ${className}`}
       style={{ color: textColor, backgroundColor }}
     >
       {pillText}
--- src/components/SpotlightImage.tsx
+++ src/components/SpotlightImage.tsx
@@ -14,23 +14,23 @@ export const SpotlightImage = ({
   parkDescription,
 }: SpotlightImageProps) => {
   return (
-    <div className="relative w-full max-w-[800px] aspect-[4/3] flex flex-col justify-end ">
+    <div className="relative w-full max-w-[800px] aspect-[4/3] flex flex-col justify-end rounded-3xl overflow-hidden">
       <Image
         src={parkImage}
         alt={parkName}
         fill
-        className="object-cover rounded-3xl"
+        className="object-cover"
         priority
       />
 
       {/* Backdrop overlay */}
-      <div className="absolute inset-0 bg-black/50" />
+      <div className="absolute inset-0 bg-gradient-to-t from-black/90 via-black/50 to-black/10"></div>
 
       {/* Overlay content */}
-      <div className="absolute right-4 z-50 bg-opacity-90 rounded-xl p-4 flex flex-col gap-4">
+      <div className="absolute right-4 z-50 bg-opacity-90 p-8 flex flex-col">
         {/* Top Row */}
         <div className="flex flex-row justify-between items-end gap-4">
-          <div className=" p-2 text-xs rounded font-playfairDisplay uppercase">
+          <div className=" p-2 text-xs font-playfairDisplay uppercase">
             <Pill
               pillText="Most Popular"
               backgroundColor="#8f663d"
@@ -58,37 +58,34 @@ export const SpotlightImage = ({
             >
               {" "}
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M14 5l7 7m0 0l-7 7m7-7H3"
               ></path>{" "}
             </svg>
           </button>
         </div>
 
-        {/* Divider */}
-        <hr className="border-t border-gray-300" />
-
         {/* Stats Row */}
-        <div className="grid grid-cols-3 gap-2">
-          <div className="px-2 py-2 items-center bg-amber-300 rounded text-center">
+        <div className="grid grid-cols-3 gap-2 border-t border-white/20 pt-6 mt-8">
+          <div>
             <div className="font-playfairDisplay text-[#8f663d] font-bold text-3xl font-oldstyle-nums">
               1890
             </div>
-            <div className="text-sm">Year Established</div>
+            <div className="text-sm text-white/60">Year Established</div>
           </div>
-          <div className="px-2 py-2 items-center bg-amber-600 rounded text-center">
+          <div>
             <div className="font-playfairDisplay text-[#8f663d] font-bold text-3xl oldstyle-nums">
               1,200+
             </div>
-            <div className="text-sm">Square Miles</div>
+            <div className="text-sm text-white/60">Square Miles</div>
           </div>
-          <div className="px-2 py-2 items-center bg-amber-300 rounded text-center">
+          <div>
             <div className="font-playfairDisplay text-[#8f663d] font-bold text-3xl oldstyle-nums">
               4M+
             </div>
-            <div className="text-sm">Annual Visitors</div>
+            <div className="text-sm text-white/60">Annual Visitors</div>
           </div>
         </div>
       </div>
